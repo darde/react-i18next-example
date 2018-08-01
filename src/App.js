eb664995e8160feaa342b0ab41c31f3a00b40993
darde <pablodarde@gmail.com>
2018-07-31 22:10:50 -0300

    Creating an i18n provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import AppProvider from './components/context/AppProvider';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import Router from './components/Router';
 import Toolbar from './components/Toolbar';
 import './styles/styles.css';
 
 const App = () => (
   <div className='wrapper'>
-    <AppProvider>
-      <Toolbar />
-      <Router />
-    </AppProvider>
+    <I18nextProvider i18n={i18n}>
+      <AppProvider>
+        <Toolbar />
+        <Router />
+      </AppProvider>
+    </I18nextProvider>
   </div>
 );
 

341a637b412a14a5a52802b2c744b63f5a859e51
darde <pablodarde@gmail.com>
2018-08-04 21:28:04 -0300

    Improve locale files

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,36 +1,14 @@
 import i18n from 'i18next';
-import pt from './locales/default.json';
-import en from './locales/default.en.json';
-import homePt from '../components/Home/locales/default.json';
-import homeEn from '../components/Home/locales/default.en.json';
-import augustinePt from '../components/lighthouses/Augustine/locales/default.json';
-import augustineEn from '../components/lighthouses/Augustine/locales/default.en.json';
-import herculesPt from '../components/lighthouses/Hercules/locales/default.json';
-import herculesEn from '../components/lighthouses/Hercules/locales/default.en.json';
-import lindauPt from '../components/lighthouses/Lindau/locales/default.json';
-import lindauEn from '../components/lighthouses/Lindau/locales/default.en.json';
-import peggysPt from '../components/lighthouses/Peggys/locales/default.json';
-import peggysEn from '../components/lighthouses/Peggys/locales/default.en.json';
-
-pt['pt-BR'].home = homePt['pt-BR'];
-pt['pt-BR'].lighthouses.augustine = augustinePt;
-pt['pt-BR'].lighthouses.hercules = herculesPt;
-pt['pt-BR'].lighthouses.lindau = lindauPt;
-pt['pt-BR'].lighthouses.peggys = peggysPt;
-en['en'].home = homeEn['en'];
-en['en'].lighthouses.augustine = augustineEn;
-en['en'].lighthouses.hercules = herculesEn;
-en['en'].lighthouses.lindau = lindauEn;
-en['en'].lighthouses.peggys = peggysEn;
+import { pt, en } from './locales';
 
 const options = {
   interpolation: {
     escapeValue: false, // not needed for react!!
   },
-  
-  lng: 'en',
 
   debug: true,
+
+  lng: 'en',
 
   resources: {
     pt: {
@@ -53,6 +31,5 @@ const options = {
 };
 
 i18n.init(options);
-
 
 export default i18n;
